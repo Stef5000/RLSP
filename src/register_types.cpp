3b08b52419b6a0b10276936b7c02d99f34b96288
Stef <48965496+Stef5000@users.noreply.github.com>
2025-09-29 19:08:17 +0200

    Update register_types.cpp

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,7 +1,7 @@
 // Include your classes, that you want to expose to Godot
 #include "ppo.h"
 #include "ppoc.h"
-//#include "dqn.h"
+#include "dqn.h"
 
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
@@ -18,7 +18,7 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 
 	GDREGISTER_CLASS(PPO)
 	GDREGISTER_CLASS(PPOC)
-	//GDREGISTER_CLASS(DQN)
+	GDREGISTER_CLASS(DQN)
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
